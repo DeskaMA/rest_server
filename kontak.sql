c1c5958dc86e9fae16bcef7243f06b79418295fa
DeskaMA <mdeska29@gmail.com>
2020-12-17 10:18:57 +0700

    deska

---
+++ kontak.sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 17 Des 2020 pada 03.46
+-- Versi server: 10.1.34-MariaDB
+-- Versi PHP: 7.0.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kontak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `telepon`
+--
+
+CREATE TABLE `telepon` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `nomor` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `telepon`
+--
+
+INSERT INTO `telepon` (`id`, `nama`, `nomor`) VALUES
+(1, 'Asep', '08576666762'),
+(2, 'Dani', '08576666770'),
+(3, 'Bambang', '085766123345'),
+(4, 'Jaya', '085766666954'),
+(5, 'ani', '08576666784'),
+(6, 'aishah', '08576666521'),
+(7, 'lana', '08576666234'),
+(8, 'sandi', '08576666869'),
+(9, 'dana', '08576666865'),
+(10, 'nanda', '08576666556'),
+(11, 'risa', '085766612321');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `telepon`
+--
+ALTER TABLE `telepon`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `telepon`
+--
+ALTER TABLE `telepon`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
